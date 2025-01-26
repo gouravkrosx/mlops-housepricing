99af3ffd7cf8bfb1f4c9da8186eb1a6d8514b00f
Gourav kumar <44055698+gouravkrosx@users.noreply.github.com>
2025-01-26 18:52:07 +0530

    Delete test/test_main.py

--- test/test_main.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from main import *  # Importing all from the source module
-def test_dummy():
-	assert True
-# Test generated using Keploy
-def test_add_numbers_positive_integers():
-    result = add_numbers(3, 5)
-    assert result == 8, "Expected 3 + 5 to equal 8"
